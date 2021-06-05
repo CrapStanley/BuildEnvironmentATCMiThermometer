8e90c20d9fd74139a1d17577d5603b131533320e
CrapStanley <wingi1@msn.com>
2021-06-05 20:59:03 +0100

    Update 00_bootstrap.sh

--- 00_bootstrap.sh
+++ 00_bootstrap.sh
@@ -10,4 +10,4 @@ git clone --depth=1 https://github.com/Ai-Thinker-Open/Telink_825X_SDK SDK
 
 
 echo \#\# Download ATC_MiThermometer project
-git clone https://github.com/atc1441/ATC_MiThermometer
+git clone https://github.com/CrapStanley/ATC_MiThermometer
